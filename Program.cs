075f3e22bf0d511fb1883030b68d569ba423a7c1
lejin2000 <lejin2000@hotmail.com>
2020-06-29 16:27:35 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using GeometryTool;
+using System;
+
+namespace GeometryTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var square = new Square() { Width = 2 };
+            var triangle = new Triangle() { Base = 2, Height = 5};
+            // Console.WriteLine(square.GetArea());
+            //Console.WriteLine(triangle.GetArea());
+            square.Display();
+            triangle.Display();
+        }
+    }
+
+    abstract class Shape
+    {
+        public abstract int GetArea();
+
+        public void Display()
+        {
+            Console.WriteLine("This area is {0}", GetArea());
+        }
+    }
+
+    class Square : Shape
+    {
+        public int Width;
+
+        public override int GetArea()
+        {
+            return Width * Width;
+        }
+    }
+
+    class Triangle : Shape
+    {
+        public int Base;
+        public int Height;
+
+        public override int GetArea()
+        {
+            return Base * Height / 2;
+        }
+
+    }
+}
